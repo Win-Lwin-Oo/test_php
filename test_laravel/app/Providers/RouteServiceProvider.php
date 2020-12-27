e526e5a1757bff2e66cd32764883c80bda345966
win <winlwinoocu@gmail.com>
2020-12-28 00:10:52 +0630

    add new Controller

--- test_laravel/app/Providers/RouteServiceProvider.php
+++ test_laravel/app/Providers/RouteServiceProvider.php
@@ -26,7 +26,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string|null
      */
-    // protected $namespace = 'App\\Http\\Controllers';
+    protected $namespace = 'App\\Http\\Controllers'; // fixed laravel V-8
 
     /**
      * Define your route model bindings, pattern filters, etc.
--- test_laravel/routes/web.php
+++ test_laravel/routes/web.php
@@ -13,6 +13,35 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+// URL Pattern
+// -> /resource/action/id
+// -> /resource/action/id/sub-resource/sub-action
+// -> don't use underscore(_)
+// -> use plural case (eg: articles, syudents)
+// -> action (eg: add, update, delete, view, detail)
+// -> unique identifier (id)
+
+// URL Query
+// -> eg: /students?location=ygn&sex=female
+
+Route::get('/', 'ArticleController@index');
+
+Route::get('/articles', 'ArticleController@index');
+
+Route::get('/articles/detail/{id}', 'ArticleController@detail');
+
+Route::post('/articles/detail', function () {
+    return 'Article detail';
 });
+
+Route::get('/articles/more', function () {
+    return redirect()->route('articles.detail'); // call route name to redirect
+});
+
+Route::get('/articles/detail', function () {
+    return 'Article more detail';
+})->name('articles.detail'); // set route name
