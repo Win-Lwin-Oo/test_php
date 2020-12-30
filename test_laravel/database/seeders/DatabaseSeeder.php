f1b1891e7f00a7cc4d9735c029facb90b2008084
Win Lwin Oo <winlwinoocu@gmail.com>
2020-12-30 10:50:33 +0630

    test relationship

--- test_laravel/database/seeders/DatabaseSeeder.php
+++ test_laravel/database/seeders/DatabaseSeeder.php
@@ -13,6 +13,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-         \App\Models\Article::factory(1)->create();
+         \App\Models\Article::factory(20)->create();
+         \App\Models\Category::factory(5)->create();
+         \App\Models\Comment::factory(20)->create();
     }
 }
